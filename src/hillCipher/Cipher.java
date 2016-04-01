54bd6a9e9663904c7bab9c53d62b918acc5acc17
Thomas Fanella <fanella@purdue.edu>
2016-04-01 19:32:46 -0400

    Restructure

---
+++ src/hillCipher/Cipher.java
@@ -0,0 +1,12 @@
+package hillCipher;
+
+public class Cipher {
+
+	public static void main(String[] args) {
+		int[][] matrixA = new int[2][2];
+		int[][] matrixV;
+		
+		System.out.print("Welcome to the Hill Cipher\nPlease input your key: ");
+		
+	}
+}
